5e1cb7f9c00a3bf53499c581dbc97cf40d1de541
Cybersavvy <iminsioluwadedunmoye23@gmail.com>
2024-09-11 11:47:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+var seriousStudent = "Favour"
+
+if (seriousStudent == 'Mariam') {
+    console.log('yesss');
+
+    seriousStudent == 'Mariam'
+    "Favour" == 'Mariam'
+    
+}
+// Buka la hot
+var isThereSwallow = true
+var swallow = "Fufu"
+var drink = 'Pure water'
+
+// Joy sent Joseph errand 
+if (isThereSwallow == true) {
+    if(swallow == "Iyan") {
+        console.log('Buy egusi');
+    } else if(swallow == 'Amala') {
+        console.log('Ewedu');
+    } else if(swallow == 'Fufu') {
+        console.log('Efo riro');
+    } else if(swallow == 'semo') {
+        console.log('okro');
+    } else {
+        console.log('agbado sisun');
+        
+    }
+} else {
+    console.log('Buy indomie, hungry woman size, 10');
+    
+}
